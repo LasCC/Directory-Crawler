5131ac3b13104e01eefec5db126439c583cb1a4e
Ludovic COULON <43119034+LasCC@users.noreply.github.com>
2020-03-23 01:21:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ import urlparse
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url", dest="url", help="Url that you want to crawl")
-    parser.add_argument("-w", "--worlist", dest="wordlist", help="Wordlist default is in the working directory", default="./wordlist.txt")
     options = parser.parse_args()
     if not options.url:
         parser.error("[!] Please add an url to proceed, --help for more informations.")
-    if not options.wordlist:
-        parser.error("[!] Please add a wordlist to proceed, --help for more informations.")
     return options
 
 def extract_all_href(url):
